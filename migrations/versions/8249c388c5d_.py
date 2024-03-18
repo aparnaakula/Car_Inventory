8377cac4c9f8471d05fe921afe40d4f6d1762274
Aparna <aparna.ramphe@gmail.com>
2024-03-18 18:10:39 -0500

    first commit

---
+++ migrations/versions/8249c388c5d_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 8249c388c5d
+Revises: 8249c388c5d9
+Create Date: 2024-03-18 15:56:51.839354
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8249c388c5d'
+down_revision = '8249c388c5d9'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ migrations/versions/9effce489e24_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 9effce489e24
+Revises: 8249c388c5d
+Create Date: 2024-03-18 15:59:13.147049
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9effce489e24'
+down_revision = '8249c388c5d'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
